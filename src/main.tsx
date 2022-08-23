b3190821e3c369aaf846abfdb4d583262cbde75f
João <joaopedronk22@gmail.com>
2022-08-23 18:12:09 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '/home/jp/Área de trabalho/dev/ReactJS_ToDoList/praticando-conceitos-reactjs/src/styles/global.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
